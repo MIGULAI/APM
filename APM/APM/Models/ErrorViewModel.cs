bd048e047628ac3294343fd690525d12203846bf
IvanVitiuk <vitukivan05@gmail.com>
2020-03-14 16:36:22 +0200

    add

---
+++ APM/APM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
